19ffd7a49ab603fb3d4b10e892347ff625ea39c5
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-06-08 14:22:18 -0400

    Update: Project now feature complete. Modal will show unique details for each project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import logo from './logo.svg';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Projects from './components/Projects';
+import Modal from './components/Modal';
 import Skills from './components/Skills';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './App.css';
 
-function App()  {
-  
-    return (
-      <div className="app">
-        <Header />
-        <Projects />
-        <Skills />
-        <About />
-        <Contact />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalContent, setModalContent] = useState({});
 
+  return (
+    <div className="app">
+      <Header />
+      <Projects setModalVisible={setModalVisible} setModalContent={setModalContent}/>
+      <Modal isOpen={modalVisible} handleClose={() => setModalVisible(false)} modalContent={modalContent} />
+      <Skills />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,38 @@
-import React, { Component} from 'react';
+import React, { Component, useState} from 'react';
 import { useInView } from 'react-intersection-observer';
 
-function Projects() {
+
+import Modal from "./Modal";
+
+function Projects(props) {
 
     
+
         const { ref, inView} = useInView({
             triggerOnce: true,
             threshold: 0,
           });
+
+          const myHorror = {
+            title: 'myHorror client', 
+            description: 'description'
+        }
+
+        const meetApp = {
+            title: "meet",
+            description: "this is meet"
+        }
   
-          
+
+          const handleClick = (event, value) => {
+            props.setModalVisible(true);
+            // setModalValue(value);
+            props.setModalContent(value); 
+          }
     
 
     return (
+        
         <section id="projects">
             <div className='row'>
                 
@@ -20,29 +40,34 @@ function Projects() {
                     <div ref={ref} className={inView ? "projects-wrapper-animate" : "projects-wrapper"}>
                         <ul className='projects-list'>
                         <li className='item-wrap'>
-                            <a href=''>
+                            
                                 <img alt='project-image' className='project-image' src='images/myHorror.png'></img>
                                 <div className='overlay'>
-                                    <div className='projects-item'>
+                                    <div  className='projects-item'>
                                         <h5>Project</h5>
                                             <p>Description</p>
+                                            
+                                            <button onClick={e => handleClick(e, myHorror)}>More Details</button>
+                                                
                                     </div>
                                 </div>
-                            </a>
+                            
                         </li>
                    
 
                     
                         <li className='item-wrap'>
-                            <a href=''>
+                            
                                 <img alt='project-image' className='project-image' src='images/myHorror.png'></img>
                                 <div className='overlay'>
                                     <div className='projects-item'>
                                         <h5>Project</h5>
                                             <p>Description</p>
+
+                                            <button onClick={e => handleClick(e, meetApp)}>More Details</button>
                                     </div>
                                 </div>
-                            </a>
+                            
                         </li>
 
                         <li className='item-wrap'>
